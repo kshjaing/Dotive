68cd8c00b1cdfe1c44e114f795ca59f2b7f6016c
kshjaing <48698567+kshjaing@users.noreply.github.com>
2020-06-02 21:48:36 +0900

    Update MainActivity.java

--- app/src/main/java/com/example/dotive/MainActivity.java
+++ app/src/main/java/com/example/dotive/MainActivity.java
@@ -16,62 +16,16 @@ import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
-    Paint[] paint;
-    double lon = 30;
-    double lat = 20;
-    int width;
-    int height;
-    int x;
-    int y;
-
-
-    String name[] = new String[]{"Joseph", "jj"};
-    Double longitude[] = new Double[]{30.2, 30.2};
-    Double latitude[] = new Double[]{30.2, 45.2};
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-
-        Panel panel = new Panel(this);
-        panel.setBackgroundColor(Color.WHITE);
-        setContentView(panel);
-
-        Display display = getWindowManager().getDefaultDisplay();  // in Activity
-        /* getActivity().getWindowManager().getDefaultDisplay() */ // in Fragment
-        Point size = new Point();
-        display.getSize(size); // or getSize(size)
-        int width = size.x;
-        int height = size.y;
     }
 
-    class Panel extends View {
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
 
-        public Panel(Context context) {
-            super(context);
-        }
+        canvas.drawCircle(x, y, radius, paint);
 
-        @SuppressLint("DrawAllocation")
-        @Override
-        public void onDraw(Canvas canvas) {
-            for (int i = 0; i < name.length; i++) {
-                paint = new Paint[i];
-                paint[i].setColor(Color.BLACK);
-                paint[i].setStrokeWidth(1);
-                paint[i].setTextSize(20);
-
-                x = (int) ((width / 360.0) * (180 + longitude[i]));
-                y = (int) ((height / 180.0) * (90 - latitude[i]));
-
-                canvas.drawColor(Color.WHITE);
-                canvas.drawCircle(x, y, 3, paint[i]);
-
-                System.out.println(x + "x" + name[i]);
-                System.out.println(y + "y" + name[i]);
-            }
-        }
     }
 }
