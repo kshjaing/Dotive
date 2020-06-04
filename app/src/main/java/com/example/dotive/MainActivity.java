2e1f9e94f9b0496c61795c8d7a26a51b3b99098b
kshjaing <48698567+kshjaing@users.noreply.github.com>
2020-06-05 00:04:27 +0900

    .

--- app/src/main/java/com/example/dotive/MainActivity.java
+++ app/src/main/java/com/example/dotive/MainActivity.java
@@ -21,14 +21,29 @@ class MyView extends View {
         setBackgroundColor(Color.WHITE);
     }
 
+    @SuppressLint("ResourceAsColor")
     @Override
     protected void onDraw(Canvas canvas) {
-        canvas.drawColor(Color.BLUE);
+        canvas.drawColor(R.color.YellowBackground);
         Paint paint = new Paint();
         paint.setColor(Color.WHITE);
         paint.setAntiAlias(true);
-        for (int i = 0; i < 14; i++) {
-                canvas.drawCircle(100 + i*100, 500, 50, paint);
+        for (int i = 0; i < 28; i++) {
+            if (i <= 7) {
+                canvas.drawCircle(100 + i * 120, 400, 50, paint);
+            }
+
+            else if (8 <= i && i <= 14) {
+                canvas.drawCircle(100 + i * 120, 520, 50, paint);
+            }
+
+            else if (15 <= i && i <= 21) {
+                canvas.drawCircle(100 + i * 120, 640, 50, paint);
+            }
+
+            else if (22 <= i && i <= 28) {
+                canvas.drawCircle(100 + i * 120, 760, 50, paint);
+            }
         }
 
     }
