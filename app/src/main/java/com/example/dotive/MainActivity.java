7ef84927707df71840b07c13ed3975b95f6ea0e3
kshjaing <48698567+kshjaing@users.noreply.github.com>
2020-06-06 17:43:32 +0900

    폰트추가및 코드수정

--- app/src/main/java/com/example/dotive/MainActivity.java
+++ app/src/main/java/com/example/dotive/MainActivity.java
@@ -18,29 +18,33 @@ import android.view.Window;
 import android.app.Activity;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        FrameLayout fl = new FrameLayout(this); //프레임레이아웃 객체 생성
-
-        FrameLayout.LayoutParams frameLayoutParams =
-                new FrameLayout.LayoutParams(
-                        FrameLayout.LayoutParams.WRAP_CONTENT,
-                        FrameLayout.LayoutParams.WRAP_CONTENT
+        RelativeLayout rl = new RelativeLayout(this); //렐러티브레이아웃 객체 생성
+        rl.setBackgroundColor(Color.parseColor("#FFF7CD"));
+        RelativeLayout.LayoutParams relativeLayoutParams =
+                new RelativeLayout.LayoutParams(
+                        RelativeLayout.LayoutParams.MATCH_PARENT,
+                        RelativeLayout.LayoutParams.MATCH_PARENT
                 );
 
         ImageButton plusimgbtn = new ImageButton(this);
         plusimgbtn.getBackground().setAlpha(0); //이미지 뒷배경 투명하게
-        plusimgbtn.setLayoutParams(frameLayoutParams);
+        plusimgbtn.setLayoutParams(relativeLayoutParams);
         plusimgbtn.setImageDrawable(getResources().getDrawable(R.drawable.plusbutton));
+        plusimgbtn.setLayoutParams(relativeLayoutParams);
+        relativeLayoutParams.addRule(RelativeLayout.CENTER_IN_PARENT);
 
         MyView w = new MyView(this);
-        fl.addView(w);
-        fl.addView(plusimgbtn);
-        setContentView(fl);
+
+        rl.addView(w);
+        rl.addView(plusimgbtn);
+        setContentView(rl);
     }
 }
 
@@ -48,13 +52,14 @@ class MyView extends View {
 
     public MyView(Context context) {
         super(context);
+
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-
-        canvas.drawColor(Color.parseColor("#FFF7CD"));
         Paint paint = new Paint();
+
+        setBackgroundColor(Color.BLACK);
         paint.setColor(Color.parseColor("#578DC2"));
         paint.setAntiAlias(true);
         for (int i = 0; i < 30; i++) {
