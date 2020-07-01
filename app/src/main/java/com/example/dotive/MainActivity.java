d0f99ccb823dc29500115f8384d6c70a3cfb2d0b
BaecMari <53397420+BaecMari@users.noreply.github.com>
2020-07-02 04:23:22 +0900

    a
    a

--- app/src/main/java/com/example/dotive/MainActivity.java
+++ app/src/main/java/com/example/dotive/MainActivity.java
@@ -429,7 +429,7 @@ public class MainActivity extends AppCompatActivity {
         ibtnEdit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+
             }
         });
 
