782e745f57f48a1e162b393685545a8ec2b89a0a
kshjaing <48698567+kshjaing@users.noreply.github.com>
2020-07-04 12:00:29 +0900

    .

--- app/src/main/java/com/example/dotive/EditActivity.java
+++ app/src/main/java/com/example/dotive/EditActivity.java
@@ -1,13 +1,239 @@
 package com.example.dotive;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import static com.example.dotive.MainActivity.db;
+import static com.example.dotive.MainActivity.isDarkmode;
 
 public class EditActivity extends Activity {
+    ConstraintLayout cl;
+    Button btnCreate;
+    EditText edtHabitName, edtObjectDays;
+    TextView txtHabit, txtObjdays;
+    ImageButton red, orange, green, blue, purple, gray;
+    Integer objectDays = 1;
+    String curColor = "red";
+    InputMethodManager imm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+
+
+        imm = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+
     }
+
+    protected  void onStart() {
+        super.onStart();
+        cl = new ConstraintLayout(this);
+        cl = findViewById(R.id.cl);
+        edtHabitName = findViewById(R.id.edtHabit);
+        edtObjectDays = findViewById(R.id.edtObjDays);
+        View view = getWindow().getDecorView();
+
+        //화면 터치시 키보드내림
+        cl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imm.hideSoftInputFromWindow(edtHabitName.getWindowToken(), 0);
+            }
+        });
+
+        //엔터키 눌렀을 때 키보드 내려갈수있게
+        edtObjectDays.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                switch (actionId) {
+                    case EditorInfo.IME_ACTION_SEARCH:
+                        // 검색 동작
+                        break;
+                    default:
+                        // 기본 엔터키 동작
+                        return false;
+                }
+                return true;
+            }
+        });
+
+
+        //-----------------------------색상 버튼들 생성 및 이벤트 설정------------------------------------
+        red = new ImageButton(this);
+        orange = new ImageButton(this);
+        green = new ImageButton(this);
+        blue = new ImageButton(this);
+        purple = new ImageButton(this);
+        gray = new ImageButton(this);
+        txtHabit = new TextView(this);
+        txtObjdays = new TextView(this);
+
+
+        red = findViewById(R.id.btncolorRed);
+        orange= findViewById(R.id.btncolorOrange);
+        green = findViewById(R.id.btncolorGreen);
+        blue = findViewById(R.id.btncolorBlue);
+        purple = findViewById(R.id.btncolorPurple);
+        gray = findViewById(R.id.btncolorBluegray);
+        txtHabit = findViewById(R.id.txtHabit);
+        txtObjdays = findViewById(R.id.txtObjdays);
+
+
+
+        red.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                curColor = "red";
+                red.setBackgroundResource(R.drawable.colorbutton_red_pressed);
+                orange.setBackgroundResource(R.drawable.colorbutton_orange);
+                green.setBackgroundResource(R.drawable.colorbutton_green);
+                blue.setBackgroundResource(R.drawable.colorbutton_blue);
+                purple.setBackgroundResource(R.drawable.colorbutton_purple);
+                gray.setBackgroundResource(R.drawable.colorbutton_bluegray);
+
+            }
+        });
+        orange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                curColor = "orange";
+                red.setBackgroundResource(R.drawable.colorbutton_red);
+                green.setBackgroundResource(R.drawable.colorbutton_green);
+                blue.setBackgroundResource(R.drawable.colorbutton_blue);
+                purple.setBackgroundResource(R.drawable.colorbutton_purple);
+                gray.setBackgroundResource(R.drawable.colorbutton_bluegray);
+                orange.setBackgroundResource(R.drawable.colorbutton_orange_pressed);
+            }
+        });
+        green.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                curColor = "green";
+                red.setBackgroundResource(R.drawable.colorbutton_red);
+                orange.setBackgroundResource(R.drawable.colorbutton_orange);
+                blue.setBackgroundResource(R.drawable.colorbutton_blue);
+                purple.setBackgroundResource(R.drawable.colorbutton_purple);
+                gray.setBackgroundResource(R.drawable.colorbutton_bluegray);
+                green.setBackgroundResource(R.drawable.colorbutton_green_pressed);
+            }
+        });
+        blue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                curColor = "blue";
+                red.setBackgroundResource(R.drawable.colorbutton_red);
+                orange.setBackgroundResource(R.drawable.colorbutton_orange);
+                green.setBackgroundResource(R.drawable.colorbutton_green);
+                purple.setBackgroundResource(R.drawable.colorbutton_purple);
+                gray.setBackgroundResource(R.drawable.colorbutton_bluegray);
+                blue.setBackgroundResource(R.drawable.colorbutton_blue_pressed);
+            }
+        });
+        purple.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                curColor = "purple";
+                red.setBackgroundResource(R.drawable.colorbutton_red);
+                orange.setBackgroundResource(R.drawable.colorbutton_orange);
+                green.setBackgroundResource(R.drawable.colorbutton_green);
+                gray.setBackgroundResource(R.drawable.colorbutton_bluegray);
+                blue.setBackgroundResource(R.drawable.colorbutton_blue);
+                purple.setBackgroundResource(R.drawable.colorbutton_purple_pressed);
+            }
+        });
+        gray.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                curColor = "gray";
+                red.setBackgroundResource(R.drawable.colorbutton_red);
+                orange.setBackgroundResource(R.drawable.colorbutton_orange);
+                green.setBackgroundResource(R.drawable.colorbutton_green);
+                purple.setBackgroundResource(R.drawable.colorbutton_purple);
+                blue.setBackgroundResource(R.drawable.colorbutton_blue);
+                gray.setBackgroundResource(R.drawable.colorbutton_bluegray_pressed);
+            }
+        });
+
+        //---------------------------------------------------------------------------------------------
+
+
+
+
+
+        //액티비티 들어올때 다크모드 체크
+        if (isDarkmode == 0) {
+            cl.setBackgroundColor(Color.parseColor("#FFEBD3"));
+            getWindow().setStatusBarColor(Color.parseColor("#FFEBD3"));
+            view.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+            txtHabit.setTextColor(Color.BLACK);
+            txtObjdays.setTextColor(Color.BLACK);
+        }
+
+        else {
+            cl.setBackgroundColor(Color.parseColor("#272B36"));
+            getWindow().setStatusBarColor(Color.parseColor("#272B36"));
+            txtHabit.setTextColor(Color.WHITE);
+            txtObjdays.setTextColor(Color.WHITE);
+        }
+
+        //습관 추가 버튼 클릭 이벤트 부여
+        btnCreate = new Button(this);
+        btnCreate = findViewById(R.id.btnCreate);
+
+        btnCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (edtHabitName.length() == 0) {
+                    Toast.makeText(EditActivity.this, "습관명을 입력해주세요!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                else if (edtObjectDays.length() == 0) {
+                    Toast.makeText(EditActivity.this, "목표일수를 입력해주세요!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (Integer.parseInt(edtObjectDays.getText().toString()) > 30) {
+                    Toast.makeText(EditActivity.this, "습관은 30일까지만 생성가능합니다", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                //목표일수 EditText에 텍스트가 있으면 그 숫자를 변수에 삽입
+                if (edtObjectDays.length() != 0) {
+                    objectDays = Integer.parseInt(edtObjectDays.getText().toString());
+                }
+
+                //습관 진행도 표시 문자열
+                String progressString = new String();
+                for (int i = 0; i < objectDays; i++) {
+                    progressString = progressString + "0";
+                }
+                Intent intent = new Intent(EditActivity.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+                MainActivity.isCreatePressed = true;
+                //EditHabits(edtHabitName.getText().toString(), curColor, objectDays, progressString);
+                db.close();
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(EditActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
+
 }
